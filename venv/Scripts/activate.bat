c4e504d4bbfa830c13cd653cad1f0aac7ec82df6
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-09-29 23:14:30 -0300

    venv changed

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\crist\Projects\WebApp\venv
+set VIRTUAL_ENV=C:\Users\crist\Projects\WebApp-in-Flask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
